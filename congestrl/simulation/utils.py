1ca046dbde62156d05b1400ac05bfbd03092f179
Emily Palaska <aimilia.p2@gmail.com>
2025-03-17 13:55:46 +0200

    Create utils.py

---
+++ congestrl/simulation/utils.py
@@ -0,0 +1,19 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def find_key(my_dict, element):
+    for key, values in my_dict.items():
+        if element in values:
+            return key
+    return None
+
+def draw_topology(graph, layout=nx.circular_layout):
+    if not layout in [nx.circular_layout,
+                      nx.spring_layout,
+                      nx.kamada_kawai_layout,
+                      nx.spectral_layout,
+                      nx.random_layout]:
+        raise TypeError
+    pos = layout(graph)
+    nx.draw(graph, pos, node_color='lightblue', node_size=5)
+    plt.show()
