3e28b41b45a80ef7dd3fa54032ed0be64169f0f2
Emily Palaska <aimilia.p2@gmail.com>
2025-04-17 18:50:42 +0300

    update weight and forwarding

---
+++ congestrl/simulation/routing.py
@@ -0,0 +1,53 @@
+import threading, time, queue
+from utils import demultiplex_packets, create_packets
+from colorama import Fore
+
+class Router:
+    def __init__(self, router_id, user_ids_map, num_users, graph):
+        self.router_id = router_id
+        self.num_users = num_users
+        self.stop_event = threading.Event()
+        self.graph = graph
+
+        self.user_ids_map = user_ids_map
+        self.connected_routers = None
+
+        self.incoming_queue = queue.Queue()
+        self.router_thread = threading.Thread(target=self.router_thread)
+        self.congestion_times = []
+
+    def start(self):
+        self.router_thread.start()
+
+    def stop(self):
+        self.stop_event.set()
+        self.router_thread.join()
+        print(Fore.YELLOW + f'Router {self.router_id} stopped')
+
+    def _forward_packets(self, routed_packets):
+        sleep_time = 0
+        for next_router_id, packets in routed_packets.items():
+            try:
+                new_weight = sum(packet["weight"] for packet in packets)
+                sleep_time += new_weight
+                self.graph[self.router_id][next_router_id]['weight'] = new_weight
+            except Exception as e:
+                print(Fore.RED +
+                      f'Iterating router {self.router_id} with next router {next_router_id}:: '
+                      + f'\nRouted packets: {routed_packets}'
+                      + f'\nException: {e}')
+            self.connected_routers[next_router_id].incoming_queue.put(packets)
+        return sleep_time
+
+    def router_thread(self):
+        while not self.stop_event.is_set():
+            if self.graph is None: continue
+
+            packets = create_packets(self.router_id, self.user_ids_map, self.graph)
+            if not self.incoming_queue.empty():
+                packets.extend(self.incoming_queue.get())
+
+            routed_packets = demultiplex_packets(self.router_id, packets)
+            sleep_time = 0 if routed_packets is None else self._forward_packets(routed_packets)
+            self.congestion_times.append(sleep_time)
+            time.sleep(sleep_time / 10)
--- congestrl/simulation/utils.py
+++ congestrl/simulation/utils.py
@@ -1,33 +1,42 @@
 import networkx as nx
-import matplotlib.pyplot as plt
 from colorama import Fore
+import random
+import numpy as np
 
-def find_key(my_dict, element):
-    for key, values in my_dict.items():
-        if element in values:
-            return key
-    return None
+def create_packets(router_id, user_ids_map, graph):
+    packets = []
+    num_users = sum(len(users) for _, users in user_ids_map.items())
+    for user_id in user_ids_map[router_id]:
+        source_node = router_id
+        destination_user = user_decide_destination(user_id, num_users)
+        if destination_user is None: continue
 
-def draw_topology(graph, layout=nx.circular_layout):
-    if not layout in [nx.circular_layout,
-                      nx.spring_layout,
-                      nx.kamada_kawai_layout,
-                      nx.spectral_layout,
-                      nx.random_layout]:
-        raise TypeError
-    pos = layout(graph)
-    nx.draw(graph, pos, node_color='lightblue', node_size=5)
-    plt.show()
+        destination_node = find_key(user_ids_map, destination_user)
+        if destination_node == router_id: continue
+        best_path = shortest_path_policy(graph, source_node, destination_node)
+
+        if best_path:
+            packet = {
+                "source_node": source_node,
+                "destination_node": destination_node,
+                "path": best_path,
+                "weight": random.randint(1, 10)
+            }
+            packets.append(packet)
+        else:
+            print(Fore.BLUE + f"Router {source_node} no path to Router {destination_node}")
+    return packets
 
 def demultiplex_packets(router_id, packets):
     if not packets: return None
-
     routed_packets = {}
+
     for packet in packets:
         if packet["destination_node"] == router_id:
-            print(Fore.GREEN + f"Router {router_id} received packet with path {packet["path"]}.")
+            #print(Fore.GREEN + f"\nRouter {router_id} received packet with path {packet["path"]}.")
             continue
 
+        # todo change the path dynamically
         path = packet["path"]
         current_index = path.index(router_id)
         next_router_id = path[current_index + 1]
@@ -36,13 +45,20 @@ def demultiplex_packets(router_id, packets):
             routed_packets[next_router_id].append(packet)
         except KeyError:
             routed_packets[next_router_id] = [packet]
-    """
-    print(Fore.MAGENTA
-          + f'\nRouter {router_id} Demultiplexing'
-          + f'\nGot: {packets}'
-          + f'\nReturning: {routed_packets}')
-    """
     return routed_packets if not routed_packets == {} else None
+
+def user_decide_destination(user_id, num_users, send_rate=0.5 , dist=np.random.normal):
+    number = dist(0, 1)
+    if send_rate > number > - send_rate:
+        destination = random.randint(0, num_users - 1)
+        if destination != user_id: return destination
+    return None
+
+def find_key(my_dict, element):
+    for key, values in my_dict.items():
+        if element in values:
+            return key
+    return None
 
 def shortest_path_policy(graph, start_node, destination_node):
     try:
