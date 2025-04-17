a454984999925ca3bcc144bd4a11e5517fedcf30
Emily Palaska <aimilia.p2@gmail.com>
2025-04-17 18:50:09 +0300

    decenter forwarding to routers

--- congestrl/simulation/dummy.py
+++
@@ -1,6 +0,0 @@
-import networkx as nx
-
-a = [1, 2, 3, 4]
-graph = nx.Graph()
-graph.add_edge(1, 2, weight=1)
-graph[1][2]['weight'] = 4
--- congestrl/simulation/graph.py
+++ congestrl/simulation/graph.py
@@ -1,23 +1,30 @@
 import networkx as nx
-from nodes import Router
+from routing import Router
 import random, time
 from colorama import Fore
+from congestrl.visualization.congestion_graph import draw_congestion_graph
 
 class NetworkTopology:
-    def __init__(self, num_users=10, num_routers=10):
+    def __init__(self, num_users=10, num_routers=10, connection_density=0.5):
         self.num_users = num_users
         self.num_routers = num_routers
         self.graph = nx.Graph()
         self.routers = None
+        self.connection_density = connection_density
 
-    def initialize_routers(self):
+        self._initialize_graph(connection_density=connection_density)
+        self._initialize_routers()
+
+    def _initialize_routers(self):
         self._assign_users_to_router()
         self.routers = [Router(router_id,
                                self.user_ids_map,
-                               self.num_users)
+                               self.num_users,
+                               self.graph)
                         for router_id in range(self.num_routers)]
+        self._assign_connected_routers()
 
-    def initialize_graph(self, connection_density=0.05):
+    def _initialize_graph(self, connection_density=0.05):
         self.graph.add_nodes_from([i for i in range(self.num_routers)])
         for i in range(self.num_routers):
             for j in range(i + 1, self.num_routers):
@@ -44,57 +51,51 @@ class NetworkTopology:
             self.user_ids_map[i] = sublist
             start = end
 
+    def _assign_connected_routers(self):
+        for i in range(self.num_routers):
+            connected_routers = {
+                self.routers[n].router_id: self.routers[n]
+                    for n in self.graph.neighbors(i)
+            }
+            self.routers[i].connected_routers = connected_routers
+
     def start(self):
-        # Start the router thread
         for router in self.routers:
             router.graph = self.graph
             router.start()
 
         start_time = time.time()
         while time.time() - start_time < 20:
-            # Forward the packets from the outgoing queues
-            for router in self.routers:
-                if not router.outgoing_queue.empty():
-                    routed_packets = router.outgoing_queue.get().items()
-
-                    for next_router_id, packets in routed_packets:
-                        try:
-                            self.graph[router.router_id][next_router_id]['weight'] = len(packets)
-                        except Exception as e:
-                            print(Fore.RED +
-                                  f'Iterating router {router.router_id} with next router {next_router_id}:: '
-                                  + f'\nRouted packets: {routed_packets}'
-                                  + f'\nException: {e}')
-
-                        self.routers[next_router_id].incoming_queue.put(packets)
-
-            for router in self.routers:
-                router.graph = self.graph
-            #print(Fore.BLUE + 'Graph updated for all users.')
-
-            #total_weight = sum(data['weight'] for u, v, data in self.graph.edges(data=True))
-            #print(Fore.CYAN + f"Total Graph weight: {total_weight}")
+            total_weight = sum(w for _, w in self.graph.edges())
+            print(Fore.CYAN + f"\nTotal Graph weight: {total_weight}")
+            time.sleep(1)
 
     def stop(self):
         for router in self.routers:
             router.stop()
 
 def main():
-    net = NetworkTopology()
-    net.initialize_routers()
-    net.initialize_graph(connection_density=0.5)
+    net = NetworkTopology(num_users=500, num_routers=50, connection_density=0.5)
 
     print('USERS MAP')
     print(net.user_ids_map)
     print('='*60)
 
+    """
     print('NETWORK')
     for edge in net.graph.edges():
         print(edge)
     print('='*60)
+    """
 
     net.start()
     net.stop()
+
+    congestion_times = {
+        router.router_id: router.congestion_times
+            for router in net.routers
+    }
+    draw_congestion_graph(congestion_times)
 
 if __name__ == '__main__':
     main()
