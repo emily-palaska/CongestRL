4712768300cf3e7896fcd9ffe2fae8013946148b
Emily Palaska <aimilia.p2@gmail.com>
2025-05-13 20:05:46 +0300

    add frozen time calculation for better sync

--- congestrl/core/packets.py
+++ congestrl/core/packets.py
@@ -1,4 +1,4 @@
-from congestrl.core.graph import shortest_path_policy
+from congestrl.core.graphs import shortest_path_policy
 from congestrl.core.statistics import probabilistic_redirect
 import random, time
 from colorama import Fore
@@ -26,18 +26,44 @@ def create_packets(router_id, local_users, num_routers, graph, send_rate=0.01):
             print(Fore.BLUE + f"Router {source_node} no path to Router {dest_node}")
     return packets
 
-def demultiplex_packets(router_id, packets):
+def calculate_delay(created, received, active_periods):
+    on_air = round(received - created, 2)
+    if not active_periods: return on_air
+
+    start_idx, end_idx = None, None
+    for i, (start, end) in enumerate(active_periods):
+        if start <= created <= end: start_idx = i
+        if start <= received <= end: end_idx = i
+
+    if start_idx is None and end_idx is None:
+        return on_air # Both timestamps on last active period
+
+    if start_idx is None:
+        start_idx = 0 # Fallback in case active_periods overflows
+
+    if end_idx is None: # Received timestamp on last active period
+        end_idx = len(active_periods) - 1
+    assert end_idx >= start_idx, f"{end_idx} < {start_idx}"
+
+    frozen = 0.0
+    if start_idx is not None and end_idx is not None and end_idx > start_idx:
+        for i in range(start_idx + 1, end_idx + 1):
+            prev_end = active_periods[i - 1][1]
+            curr_start = active_periods[i][0]
+            frozen += max(0, curr_start - prev_end)
+    return round(on_air - frozen, 2)
+
+def demultiplex_packets(router_id, packets, active_periods):
     routed_packets = {router_id: []}
     if not packets: return routed_packets
 
     for packet in packets:
         if packet["destination_node"] == router_id:
-            delay_time = round(time.time() - packet["created"], 2)
+            created, received = packet['created'], time.time()
+            delay_time = calculate_delay(created, received, active_periods)
             routed_packets[router_id].append(delay_time)
-            #print(Fore.GREEN + f"\nRouter {router_id} received packet with path {packet["path"]}.")
             continue
 
-        # todo change the path dynamically
         path = packet["path"]
         current_index = path.index(router_id)
         next_router_id = path[current_index + 1]
