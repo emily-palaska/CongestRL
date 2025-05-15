1b43c7235c979ae1e489b53593b65d42f0564e91
Emily Palaska <aimilia.p2@gmail.com>
2025-05-15 10:33:38 +0300

    update to take lists

--- congestrl/policy/rewards.py
+++ congestrl/policy/rewards.py
@@ -1,11 +1,13 @@
 
 
-def linear_reward(congestion, congestion_limit, delay, alpha=1.0, beta=1.0):
-    reward = -alpha * delay
-    if congestion < 0.6 * congestion_limit:
-        reward += beta
-    elif congestion < congestion_limit:
-        reward += 4 * beta - 5 * beta / congestion_limit * congestion
-    else:
-        reward -= 4 * beta
+def linear_reward(congestions: list, delays: list,  congestion_limit: int, alpha=1.0, beta=1.0):
+    reward = -alpha * sum(delays) / len(delays)
+
+    for c in congestions:
+        if c < 0.6 * congestion_limit:
+            reward += beta
+        elif c < congestion_limit:
+            reward += 4 * beta - 5 * beta / congestion_limit * c
+        else:
+            reward -= 4 * beta
     return reward
